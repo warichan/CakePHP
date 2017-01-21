e8c87cd8abb9a8a0574fea32f5a335582c54d84c
warichan <txaxmxmxm@gmail.com>
2017-01-21 19:54:31 +0900

    昨日(00:00:00〜23:59:59)のデータを取得し、昨日の記録ページに出力

--- diet/app/Controller/PostsController.php
+++ diet/app/Controller/PostsController.php
@@ -2,7 +2,17 @@
 
 class PostsController extends AppController{
   public function index(){
-    $this->set('posts', $this->Post->find('all'));
+    $date = new DateTime();
+    $today = $date->format('Y'.'-'.'m'.'-'.'d');
+
+    $params = array(
+      'conditions'=>array(
+        'created BETWEEN ? AND ?'=>array($today.' 00:00:00',$today.' 23:59:59')
+      )
+    );
+    $this->set('posts',$this->Post->find('all',$params));
+    //今日(00:00:00〜23:59:59)のデータを取得してpostsに渡している
+    $this->log($this->Post->getDataSource()->getLog(), LOG_DEBUG);
   }
 
   public function add() {
@@ -31,7 +41,17 @@ class PostsController extends AppController{
   }
 
   public function yesterday(){
-    
+    $date = new DateTime();
+    $date->sub(new DateInterval('P1D'));
+    $yesterday = $date->format('Y'.'-'.'m'.'-'.'d');
+
+    $params = array(
+      'conditions'=>array(
+        'created BETWEEN ? AND ?'=>array($yesterday.' 00:00:00',$yesterday.' 23:59:59')
+      )
+    );
+    $this->set('posts',$this->Post->find('all',$params));
+    //昨日(00:00:00〜23:59:59)のデータを取得してpostsに渡している
   }
 }
 
--- diet/app/View/Posts/yesterday.ctp
+++ diet/app/View/Posts/yesterday.ctp
@@ -4,3 +4,16 @@
   $date->sub(new DateInterval('P1D'));
   echo $date->format('Y'.'年'.'n'.'月'.'j'.'日');
 ?></h2>
+
+<?php foreach($posts as $post): ?>
+
+  <h3>朝</h3><p><?php echo h($post['Post']['morning']); ?></p>
+  <hr>
+  <h3>昼</h3><p><?php echo h($post['Post']['lunch']); ?></p>
+  <hr>
+  <h3>夜</h3><p><?php echo h($post['Post']['dinner']); ?></p>
+  <hr>
+  <h3>体重</h3><p><?php echo h($post['Post']['weight']); ?>kg</p>
+  <hr>
+  <h3>前日の体重比</h3><p><?php //echo h($post['Post']['difference']); ?>
+<?php endforeach; ?>
